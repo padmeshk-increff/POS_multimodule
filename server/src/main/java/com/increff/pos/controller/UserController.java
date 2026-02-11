cd027f294f9c3069715ce60f799c89e82c2e64f5
Padmesh K <padmesh.k@increff.com>
2026-02-11 19:02:19 +0530

    Stop tracking target folder

--- server/src/main/java/com/increff/pos/controller/UserController.java
+++ server/src/main/java/com/increff/pos/controller/UserController.java
@@ -1,25 +1,4 @@
 package com.increff.pos.controller;
 
-import com.increff.pos.commons.exception.ApiException;
-import com.increff.pos.dto.UserDto;
-import com.increff.pos.model.form.UserForm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.Map;
-
-@RestController
 public class UserController {
-
-    @Autowired
-    private UserDto userDto;
-
-    @RequestMapping(path = "/users/signup", method = RequestMethod.POST)
-    public Map<String, String> signup(@RequestBody UserForm userForm) throws ApiException {
-        return userDto.add(userForm);
-    }
 }
